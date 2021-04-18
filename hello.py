689f34f35af41f3a0002ab3fb02072a89faebe5d
Meera <ambaliyameera@gmail.com>
2021-04-18 16:12:10 +0530

    Hello Word Programm

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def main():
-    print("hello world!")
-    #print("hello brave new world!")
+    #print("hello world!")
+    print("hello brave new world!")
 
 if __name__ == "__main__": #if __name__ == “main”: is used to execute some code only if the file was run directly, and not imported.
     main()
